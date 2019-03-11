c50365f20e0259ab7a5a2f06e86fe89797d603e8
Артем Костельнюк <kostelnyuk@sports.ru>
2019-03-11 20:51:14 +0300

    Переписал карточки фильмов на ES-6 классы

--- src/generate-films-data.js
+++ src/generate-films-data.js
@@ -1,4 +1,44 @@
-import generateFilmData from './generate-film-data';
+import data from './mock-film-data';
+import {
+  getRandomNumber,
+  getRandomIntegerInRange,
+  getRandomBoolean,
+  getRandomArrayElement,
+  getRandomArrayElements,
+  getRandomDateInRange,
+} from './utils';
+
+const getFilmDescription = () => {
+  const amountOfSentences = getRandomIntegerInRange(4);
+  const description = getRandomArrayElements(data.descriptionSentences, amountOfSentences).join(` `);
+
+  return description;
+};
+
+const generateFilmData = () => ({
+  title: getRandomArrayElement(data.titles),
+  originalTitle: getRandomArrayElement(data.titles),
+  releaseYear: getRandomIntegerInRange(2019, 1960),
+  cast: getRandomArrayElements(data.actors, getRandomIntegerInRange(data.actors.length)),
+  description: getFilmDescription(),
+  duration: getRandomIntegerInRange(180, 60),
+  amountOfSeasons: getRandomIntegerInRange(16),
+  amountOfEpisodes: getRandomIntegerInRange(150),
+  genre: getRandomArrayElement(data.genres),
+  ageLimit: getRandomArrayElement(data.ageLimits),
+  premiereDate: getRandomDateInRange(7),
+  dvdReleaseDate: getRandomDateInRange(10),
+  rating: getRandomIntegerInRange(11, 1),
+  averageRating: getRandomNumber(11, 1).toFixed(1),
+  country: getRandomArrayElement(data.countries),
+  isFavorite: getRandomBoolean(),
+  isWatched: getRandomBoolean(),
+  inWatchlist: getRandomBoolean(),
+
+  comments: data.comments,
+  poster: getRandomArrayElement(data.posters),
+});
+
 
 // Возвращает массив длиной amount, содержащий данные о фильмах
 const generateFilmsData = (amount) => {
--- src/utils.js
+++ src/utils.js
@@ -4,29 +4,29 @@
 const getRandomNumber = (max, min = 0) => Math.random() * (max - min) + min;
 
 // Возвращает случайное целое число в диапазоне [min, max)
-const getRandomInteger = (max, min = 0) => Math.floor(Math.random() * (max - min)) + min;
+const getRandomIntegerInRange = (max, min = 0) => Math.floor(Math.random() * (max - min)) + min;
 
 // Возвращает случайный индекс массива array
-const getRandomArrayIndex = (array) => getRandomInteger(array.length);
+const getRandomArrayIndex = (array) => getRandomIntegerInRange(array.length);
 
 // Возвращает случайный элемент массива array
 const getRandomArrayElement = (array) => array[getRandomArrayIndex(array)];
 
 // Возвращает случайное булево значение
-const getRandomBoolean = () => getRandomInteger(2) === 0;
+const getRandomBoolean = () => getRandomIntegerInRange(2) === 0;
 
 // Возвращает перемешанный массив array
 const getShuffledArray = (array) => {
   const copyOfOriginalArray = [...array];
   const shuffledArray = [];
 
-  array.forEach(() => {
+  while (copyOfOriginalArray.length) {
     const randomIndex = getRandomArrayIndex(copyOfOriginalArray);
     const randomElement = copyOfOriginalArray[randomIndex];
 
     shuffledArray.push(randomElement);
     copyOfOriginalArray.splice(randomIndex, 1);
-  });
+  }
 
   return shuffledArray;
 };
@@ -34,15 +34,15 @@ const getShuffledArray = (array) => {
 // Возвращает массив длиной amountOfElements, состоящий из случайных элементов массива array
 const getRandomArrayElements = (array, amountOfElements) => getShuffledArray(array).slice(0, amountOfElements);
 
-// Возвращает случайную дату в диапазоне от текущей даты +- неделя
-const getRandomDate = () => Date.now() + getRandomInteger(8, -7) * 24 * 60 * 60 * 1000;
+// Возвращает случайную дату в диапазоне [текущая дата + min, текущая дата + max)
+const getRandomDateInRange = (max, min = 0) => Date.now() + getRandomIntegerInRange(max, min) * 24 * 60 * 60 * 1000;
 
 export {
   getRandomNumber,
-  getRandomInteger,
+  getRandomIntegerInRange,
   getRandomArrayElement,
   getRandomArrayElements,
   getRandomBoolean,
   getShuffledArray,
-  getRandomDate
+  getRandomDateInRange
 };
